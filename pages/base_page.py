76a96b9c888d13ffead5ce2c5ba466d1f2249178
IriSamo <i.samosuk@gmail.com>
2023-10-30 15:43:05 -0400

    Add conftest.py, base_page.py and test_check_structure

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def get_title(self):
+        return self.driver.title
