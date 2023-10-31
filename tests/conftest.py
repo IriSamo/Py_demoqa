00ed147296241f4e612cdf4fa6d486f04246101f
IriSamo <i.samosuk@gmail.com>
2023-10-30 23:17:19 -0400

    Ci (#6)
    * Change gitignore
    
    * Add ChromeOptions

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,14 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome()
-    driver.set_window_size(1400, 700)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    # driver.set_window_size(1400, 700)
     # driver.maximize_window()
     yield driver
     driver.quit()
