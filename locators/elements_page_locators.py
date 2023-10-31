03572ca3f3365c5e0c1260a218064776f3546d35
IriSamo <i.samosuk@gmail.com>
2023-10-31 13:22:15 -0400

    Add test_text_box and necessary page, methods, locators and data. Delete test_check_structure.

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+    FULL_NAME_FIELD = (By.ID, 'userName')
+    EMAIL_FIELD = (By.ID, 'userEmail')
+    CURRENT_ADDRESS_FIELD = (By.XPATH, '//textarea[@id = "currentAddress"]')
+    PERMANENT_ADDRESS_FIELD = (By.XPATH, '//textarea[@id = "permanentAddress"]')
+    SUBMIT_BUTTON = (By.ID, 'submit')
+
+    CREATED_FULL_NAME = (By.ID, 'name')
+    CREATED_EMAIL = (By.ID, 'email')
+    CREATED_CURRENT_ADDRESS = (By.XPATH, '//p[@id = "currentAddress"]')
+    CREATED_PERMANENT_ADDRESS = (By.XPATH, '//p[@id = "permanentAddress"]')
