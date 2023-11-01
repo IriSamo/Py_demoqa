6dfbe1b4736ce828200e74e5b8b7b9ecdbdebe38
IriSamo <i.samosuk@gmail.com>
2023-11-01 15:40:50 -0400

    Add test_check_box and necessary page, methods, locators and data. (#11)

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,8 +13,17 @@ class BasePage:
     def element_is_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
     def element_is_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
     def scroll_down(self):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
